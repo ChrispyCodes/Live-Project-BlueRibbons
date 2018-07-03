4867ca2e6f69b485b9e380d056b6b24a51080c7a
Christopher Gaynor <36244780+ChrispyCodes@users.noreply.github.com>
2018-07-03 15:26:53 -0700

    Adding all the files i worked in
    Live project files

---
+++ Wishlist.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BlueRibbonsReview.Models
+{
+    public class Wishlist
+    {
+        public int WishlistId { get; set; }
+        public int CampaignId { get; set; }
+        public string UserId { get; set; }
+
+        public virtual ApplicationUser ApplicationUser { get; set; }
+        public virtual Campaign Campaign { get; set; }
+    }
+}
---
+++ WishlistController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BlueRibbonsReview.Models;
+
+namespace BlueRibbonsReview.Controllers
+{
+    public class WishlistController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Wishlist
+        public ActionResult Index()
+        {
+            var wishlists = db.Wishlists.Include(w => w.Campaign);
+            return View(wishlists.ToList());
+        }
+
+        // GET: Wishlist/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Wishlist wishlist = db.Wishlists.Find(id);
+            if (wishlist == null)
+            {
+                return HttpNotFound();
+            }
+            return View(wishlist);
+        }
+
+        // GET: Wishlist/Create
+        public ActionResult Create()
+        {
+            ViewBag.CampaignId = new SelectList(db.Campaigns, "CampaignID", "ASIN");
+            return View();
+        }
+
+        // POST: Wishlist/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "WishlistId,CampaignId,UserId")] Wishlist wishlist)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Wishlists.Add(wishlist);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CampaignId = new SelectList(db.Campaigns, "CampaignID", "ASIN", wishlist.CampaignId);
+            return View(wishlist);
+        }
+
+        // GET: Wishlist/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Wishlist wishlist = db.Wishlists.Find(id);
+            if (wishlist == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CampaignId = new SelectList(db.Campaigns, "CampaignID", "ASIN", wishlist.CampaignId);
+            return View(wishlist);
+        }
+
+        // POST: Wishlist/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "WishlistId,CampaignId,UserId")] Wishlist wishlist)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(wishlist).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CampaignId = new SelectList(db.Campaigns, "CampaignID", "ASIN", wishlist.CampaignId);
+            return View(wishlist);
+        }
+
+        // GET: Wishlist/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Wishlist wishlist = db.Wishlists.Find(id);
+            if (wishlist == null)
+            {
+                return HttpNotFound();
+            }
+            return View(wishlist);
+        }
+
+        // POST: Wishlist/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Wishlist wishlist = db.Wishlists.Find(id);
+            db.Wishlists.Remove(wishlist);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
